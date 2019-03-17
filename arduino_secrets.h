82d6a4af1fd0ae2485a207a69b724ee5db365c6e
lincoln <lincoln@shrekware.com>
2019-03-17 00:02:59 -0400

    Create arduino_secrets.h
    added the secrets file to hold the wifi info and API keys

---
+++ arduino_secrets.h
@@ -0,0 +1,11 @@
+// this file is to keep your user names/passwords and api keys secret/separate from the main file
+// copyrite @shrekware 2019
+// please enter your wifi SSID and Password
+// please enter your carriots account information 
+// WIFI Information
+#define SECRET_SSID "replace with your wifi SSID"                  // wifi name goes here
+#define SECRET_PASS "replace with the password for the wifi"       // wifi password goes here
+
+//shrekware carriot api key
+#define CARRIOTSAPIKEY  "Replace with your Carriots apikey"           // Replace with your Carriots apikey
+#define CARRIOTSDEVICE  "Replace with the id_developer of your device" // Replace with the id_developer of your device
