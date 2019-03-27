f3304c67f8e95147aaccfdeed5e7817c223aca39
lincoln <lincoln@shrekware.com>
2019-03-26 22:54:37 -0400

    Update arduino_secrets.h

--- arduino_secrets.h
+++ arduino_secrets.h
@@ -1,12 +1,13 @@
 // this file is to keep your user names/passwords and api keys secret/separate from the main file
 // copyrite @shrekware 2019
 // please enter your wifi SSID and Password
-// please enter your carriots account information 
-// WIFI Information
+// please enter your (formerly)Carriots.com (now https://www.altairsmartworks.com/login)
+
+// ***ADD WIFI Information below***
 #define SECRET_SSID "replace with your wifi SSID"                  // wifi name goes here
 #define SECRET_PASS "replace with the password for the wifi"       // wifi password goes here
 
 //(formerly)Carriots.com (now https://www.altairsmartworks.com/login)
-// ADD your API KEY and Device ID below
+// ***ADD your API KEY and Device ID below***
 #define CARRIOTSAPIKEY  "Replace with your Carriots apikey"           // Replace with your Carriots apikey
 #define CARRIOTSDEVICE  "Replace with the id_developer of your device" // Replace with the id_developer of your device
